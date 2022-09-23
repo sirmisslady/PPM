5c5cfb68f5fadeec6ce762da18a6aa04dc36a12b
Francisco Javier <francisjavierlegaspy@gmail.com>
2022-09-23 06:45:54 -0700

    Added Fields to Django models

--- project/models.py
+++ project/models.py
@@ -11,7 +11,10 @@ class Phone(models.Model):
         return self.number
 
 class Paciente(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=50)
+    apellido_pat = models.CharField(max_length=50,default=None)
+    apellido_mat = models.CharField(max_length=50,default=None)
+    direccion = models.CharField(max_length=200,default=None)
     paciente_phones = models.ManyToManyField(Phone, through='PacientePhone')
 
     def __str__(self):
